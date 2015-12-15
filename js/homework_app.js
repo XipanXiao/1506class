d200836ab8dd342c858e1ea8c9a009c8a3a60ead
XipanXiao <XipanXiao@users.noreply.github.com>
2015-12-14 16:30:42 -0800

    Delete homework_app.js
    remove homework_app.js

--- js/homework_app.js
+++
@@ -1,7 +0,0 @@
-require(["app_bar/app_bar", "tasks/tasks", "schedule_tasks/schedule_tasks"], function() {
-	angular.module('AppModule', ['AppBarModule', 'TasksModule', 'ScheduleTasksModule']);
-
-	angular.element(document).ready(function() {
-		angular.bootstrap(document, ['AppModule']);
-	});
-});
