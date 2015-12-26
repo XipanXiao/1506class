63f31c278adad1ed7f4d2034b144cfe9f746e774
Xipan Xiao <xipanxiao@xxp-macbookpro.roam.corp.google.com>
2015-12-26 11:17:57 -0800

    add a year picker

--- js/classes/classes.js
+++ js/classes/classes.js
@@ -1,33 +1,42 @@
-define(['services'], function() {
-	return angular.module('ClassesModule', ['ServicesModule'])
-		.controller('ClassesController', function($scope, $rootScope, rpc) {
-				rpc.get_classes().then(function(response) {
-					$scope.classes = response.data;
-					
-					$scope.selected = function (id) {
-						$scope.selectedId = id;
-						$rootScope.$broadcast('class-selected', $scope.classes[id]);
-					};
+define(['services', 'utils'], function() {
+  return angular.module('ClassesModule', ['ServicesModule', 'UtilsModule'])
+    .controller('ClassesController', function($scope, $rootScope, rpc, utils) {
+        rpc.get_classes().then(function(response) {
+          $scope.alumnis = utils.groupBy(response.data, 'start_year');
+          $scope.years = utils.keys($scope.alumnis).map(parseInt); 
+          
+          $scope.yearChanged = function() {
+            $scope.classes = $scope.alumnis[$scope.year];
+          };
 
-					$scope.selectUserClass();
-				});
+          $scope.year = (new Date()).getFullYear();
+          $scope.yearChanged();
+          
+          $scope.selected = function (id) {
+            $scope.selectedId = id;
+            $rootScope.$broadcast('class-selected', $scope.classes[id]);
+          };
 
-				$scope.selectUserClass = function(classId) {
-				  if (!$scope.selected || !$scope.classId) return;
-          $scope.selected($scope.classId);
-				}
-				$scope.$on('user-loaded', function(event, user) {
-				  $scope.classId = user.classId;
           $scope.selectUserClass();
-	      });
-			})
-		.directive('classes', function() {
-			return {
-				link: function(scope, element, attributes) {
-				    scope.listType = attributes["listType"];
-				    scope.classSelected = attributes['classSelected'];
-				},
-				templateUrl : 'js/classes/classes.html'
-			};
-		});
+        });
+
+        $scope.selectUserClass = function(classId) {
+          if (!$scope.selected || !$scope.classId) return;
+          $scope.selected($scope.classId);
+        }
+
+        $scope.$on('user-loaded', function(event, user) {
+          $scope.classId = user.classId;
+          $scope.selectUserClass();
+        });
+      })
+    .directive('classes', function() {
+      return {
+        link: function(scope, element, attributes) {
+            scope.listType = attributes["listType"];
+            scope.classSelected = attributes['classSelected'];
+        },
+        templateUrl : 'js/classes/classes.html'
+      };
+    });
 });
---
+++ js/utils.js
@@ -0,0 +1,50 @@
+define(function() {
+  if (!String.prototype.format) {
+    String.prototype.format = function() {
+      var args = arguments;
+      return this.replace(/{(\d+)}/g, function(match, number) {
+        return typeof args[number] != 'undefined' ? args[number]
+            : match;
+      });
+    };
+  }
+
+  if (!Array.prototype.map) {
+    Array.prototype.map = function(callback) {
+      var result = [];
+      for (var idx in this) {
+        result[idx] = callback(this[idx]);
+      }
+      
+      return result;
+    };
+  }
+
+  return angular.module('UtilsModule', []).factory('utils', function() {
+    return {
+      keys: function(map) {
+        var result = [];
+        for (var key in map) {
+          result.push(key);
+        }
+        
+        return result;
+      },
+      groupBy: function(arr, key) {
+        var groups = {};
+        for (var idx in arr) {
+          var item = arr[idx];
+          var value = item[key];
+          var group = groups[value];
+          if (!group) {
+            group = groups[value] = [];
+          }
+          
+          group.push(item);
+        }
+        
+        return groups;
+      }
+    };
+  });
+});
