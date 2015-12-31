b9792a7148895dc6d61fe7e30fbe995a3968ecf8
Xipan Xiao <xipanxiao@xxp-macbookpro.roam.corp.google.com>
2015-12-31 14:35:58 -0800

    switch class years

--- js/classes/classes.js
+++ js/classes/classes.js
@@ -10,33 +10,38 @@ define(['services', 'utils'], function() {
           rpc.get_classes().then(function(response) {
             $scope.alumnis = utils.groupBy(response.data, 'start_year');
             $scope.years = utils.map(utils.keys($scope.alumnis), parseInt);
+
+            $scope.currentClass = {
+                year: $scope.classId ?
+                    parseInt(response.data[$scope.classId].start_year) :
+                        (new Date()).getFullYear(),
+                id: $scope.classId
+            };
             
             $scope.yearChanged = function() {
-              $scope.classes = $scope.alumnis[$scope.year];
-              $scope.selectClass();
+              $scope.classes = $scope.alumnis[$scope.currentClass.year];
+              $scope.classIds = utils.keys($scope.classes);
             };
 
-            $scope.year = (new Date()).getFullYear();
             $scope.yearChanged();
           });
 
-          $scope.selected = function (classInfo) {
-            $scope.classId = classInfo.id;
-            $scope.selectedClass = classInfo;
-            $rootScope.$broadcast('class-selected', classInfo);
+          $scope.select = function (id) {
+            $scope.classId = id;
+            $scope.currentClass.id = id;
           };
 
-          $scope.selectClass = function() {
-            if (!$scope.classes || !$scope.classId) return;
-            $scope.selected($scope.classes[$scope.classId]);
-          };
-
-          if (!$scope.classId) {
-            $scope.$on('user-loaded', function(event, user) {
-              $scope.classId = user.classId;
-              $scope.selectClass();
-            });
-          }
+//          $scope.selectClass = function() {
+//            if (!$scope.classes || !$scope.classId) return;
+//            $scope.selected($scope.classes[$scope.classId]);
+//          };
+//
+//          if (!$scope.classId) {
+//            $scope.$on('user-loaded', function(event, user) {
+//              $scope.classId = user.classId;
+//              $scope.selectClass();
+//            });
+//          }
         },
         templateUrl : 'js/classes/classes.html'
       };
--- js/learning_records/learning_records.js
+++ js/learning_records/learning_records.js
@@ -1,25 +1,29 @@
 define(['services'], function() {
   return angular.module('LearningRecordsModule', ['ServicesModule'])
-    .controller('LearningRecordsController', function($scope, $attrs, rpc) {
-      $scope.attendOptions = ['缺席', '出席', '请假'];
-      $scope.$on('class-selected', function(event, classInfo) {
-        rpc.get_learning_records(classInfo.id).then(function(response) {
-          $scope.schedules_groups = response.data.groups;
-          $scope.users = response.data.users;
-          $scope.attendence = $attrs.attendence;
-        });
-      });
-      
-      $scope.reportTask = function(user, course_id) {
-        var record = user.records[course_id];
-        record.student_id = user.id;
-        record.course_id = course_id;
-        rpc.report_schedule_task(record);
-      };
-    })
     .directive('learningRecords',
-        function() {
+        function(rpc) {
           return {
+            scope: {
+              attendence: '@',
+              classId: '='
+            },
+            link: function($scope) {
+              $scope.attendOptions = ['缺席', '出席', '请假'];
+
+              $scope.$watch('classId', function() {
+                rpc.get_learning_records($scope.classId).then(function(response) {
+                  $scope.schedules_groups = response.data.groups;
+                  $scope.users = response.data.users;
+                });
+              });
+              
+              $scope.reportTask = function(user, course_id) {
+                var record = user.records[course_id];
+                record.student_id = user.id;
+                record.course_id = course_id;
+                rpc.report_schedule_task(record);
+              };
+            },
             templateUrl : 'js/learning_records/learning_records.html'
           };
         });
