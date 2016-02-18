930d15393e6340c870850f036a47d679767cb86c
Xipan Xiao <xipanxiao@xxp-macbookpro.roam.corp.google.com>
2016-02-17 22:40:35 -0800

    add config.php

---
+++ data/config.php
@@ -0,0 +1,18 @@
+{   "nothing" : "<?php /* This makes the following content invisible for web access",
+	"localhost": {
+		"servername": "127.0.0.1",
+		"username": "root",
+		"password": "1234567890",
+		"dbname": "buddcourses",
+		"pet": "Xiaobai"
+	},
+	"buddcourses-bicw.rhcloud.com": {
+		"servername": "${OPENSHIFT_MYSQL_DB_HOST}",
+		"username": "${OPENSHIFT_MYSQL_DB_USERNAME}",
+		"password": "${OPENSHIFT_MYSQL_DB_PASSWORD}",
+		"dbname": "buddcourses",
+		"pet": "buzhanguo",
+		"session_path": "${OPENSHIFT_TMP_DIR}"
+	},
+	"closing": "*/?>"
+}
