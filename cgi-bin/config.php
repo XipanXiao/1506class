d0854a6c91665af485bbe2e4a87226e425bb7fa2
Xipan Xiao <xipanxiao@xxp-macbookpro.roam.corp.google.com>
2016-02-17 22:21:33 -0800

    reading environment variables for configures

--- cgi-bin/config.php
+++ cgi-bin/config.php
@@ -1,34 +1,50 @@
 <?php
-	// This file must be include first if session is needed since it
-	// outputs a cookie to the header.
-	$config = null;
-	
- 	function read_config() {
- 		global $config;
- 		
- 		if ($config) {
- 			return $config;
- 		}
- 		
- 		if (isset($_SESSION['config'])) {
- 			$config = unserialize($_SESSION['config']);
- 			return $config;
- 		}
- 		
- 	 	$config = json_decode(file_get_contents('../data/config.php'));
- 	 	
- 		$test = ($_SERVER['HTTP_HOST'] == 'localhost');
+  // This file must be include first if session is needed since it
+  // outputs a cookie to the header.
+  $config = null;
+  
+  /// Given "${HOSTNAME}", return getenv("HOSTNAME");
+  function replace_variables($subject) {
+    return preg_replace_callback("/\\$\\{(\w+)\\}/", function($matches) {
+      return getenv($matches[1]);
+    }, $subject);
+  }
+  
+  function replace_config_variables($config) {
+    $result = new stdClass();
+    foreach (((array)$config) as $key => $value) {
+      $result->$key = replace_variables($value);
+    }
+    
+    return $result;
+  }
+  
+  function read_config() {
+    global $config;
+    
+    if ($config) {
+      return $config;
+    }
+    
+    if (isset($_SESSION['config'])) {
+      $config = unserialize($_SESSION['config']);
+      return $config;
+    }
+     
+    $config = (array)json_decode(file_get_contents('../data/config.php'));
+      
+    $config = empty($config[$_SERVER['HTTP_HOST']]) ?
+        $config["localhost"] : $config[$_SERVER['HTTP_HOST']];
+    
+    $config = replace_config_variables($config);
 
- 		if ($test) {
- 			$config = $config->test;
- 		} else {
- 			$config = $config->production;
- 			session_save_path($config->session_path);
- 		}
- 		
- 		session_start();
- 		return $config;
- 	}
+    if ($config->session_path) {
+      session_save_path($config->session_path);
+    }
+     
+    session_start();
+    return $config;
+  }
 
- 	read_config();
+  read_config();
 ?>
