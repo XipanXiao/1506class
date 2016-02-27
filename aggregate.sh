f7b557befa0ee6782cf61d59d03a6ea7ccc964c7
Xipan Xiao <xipanxiao@xxp-macbookpro.roam.corp.google.com>
2016-02-27 08:51:16 -0800

    redirect to https

--- aggregate.sh
+++ aggregate.sh
@@ -3,4 +3,6 @@ rm -f admin.js
 for f in `cat admin.deps`; do cat $f >> admin.js; done;
 rm -f index.js
 for f in `cat index.deps`; do cat $f >> index.js; done;
-
+echo "RewriteEngine on" > .htaccess
+echo "RewriteCond %{HTTP:X-Forwarded-Proto} !https" >> .htaccess
+echo "RewriteRule .* https://%{HTTP_HOST}%{REQUEST_URI} [R,L]" >> .htaccess
