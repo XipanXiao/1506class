cbd2823a6213884e7fbc812052b845f72f05a609
Xipan Xiao <xipanxiao@xxp-macbookpro.roam.corp.google.com>
2016-06-17 23:46:15 -0700

    disable some unused php files

--- cgi-bin/update.php
+++ cgi-bin/update.php
@@ -1,4 +1,5 @@
 <?php
+/*
 include_once "config.php";
 include_once "connection.php";
 include_once "tables.php";
@@ -28,4 +29,5 @@ if (empty($_SESSION["user"])) {
   
   $conn->close();
 }
+*/
 ?>
--- cgi-bin/update_states.php
+++ cgi-bin/update_states.php
@@ -1,4 +1,5 @@
 <?php
+/*
 include_once "config.php";
 include_once "connection.php";
 include_once "tables.php";
@@ -97,4 +98,5 @@ if (empty($_SESSION["user"])) {
   	}
   }
 }
+*/
 ?>
