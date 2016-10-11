1ed97e983e40e01a183f3286eda69fb38a864132
Xipan Xiao <xxp9@hotmail.com>
2016-10-10 21:07:47 -0700

    add a class year filter for local users

--- js/local_app.js
+++ js/local_app.js
@@ -1,6 +1,7 @@
 define('local_app', [
     'app_bar/app_bar',
     'address_filter/address_filter',
+    'checkbox_filter/checkbox_filter',
     'users/users',
     'services',
     'permission',
@@ -10,6 +11,7 @@ define('local_app', [
   angular.module('AppModule', [
       'AppBarModule',
       'AddressFilterModule',
+      'CheckboxFilterModule',
       'UsersModule',
       'PermissionModule',
       'ServicesModule',
@@ -28,8 +30,39 @@ define('local_app', [
               $scope.user = user;
             });
             $scope.$on('users-selected', function(event, users) {
+              $scope.allUsers = users;
               $scope.users = users;
+              $scope.aggregateClassYears();
             });
+            $scope.classYearChanged = function() {
+              $scope.users = utils.where($scope.allUsers, function(user) {
+                var classYear = $scope.classes[user.classId].start_year;
+                return $scope.classYears[classYear].checked;
+              });
+            };
+
+            /// Initializes the [classYears] list by aggregating over all
+            /// [users].
+            $scope.aggregateClassYears = function() {
+              var classIds = {};
+              utils.forEach($scope.users, function(user) {
+                classIds[user.classId] = user.classId;
+              });
+
+              rpc.get_classes().then(function(response) {
+                var classYears = {};
+                utils.forEach(classIds, function(classId) {
+                  $scope.classes = response.data;
+                  var start_year = response.data[classId].start_year;
+                  if (classYears[start_year]) return;
+                  classYears[start_year] = {
+                    label: start_year,
+                    checked: true
+                  };
+                });
+                $scope.classYears = classYears;
+              });
+            };
           }
         };
       }).config( ['$compileProvider', function( $compileProvider ) {
