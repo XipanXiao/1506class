4b75674cb332e57e37c13c18ee068e9b2a6be2c3
Xipan Xiao <xxp9@hotmail.com>
2017-02-03 23:26:08 -0800

    null check for departments

--- js/setup_tasks/setup_tasks.js
+++ js/setup_tasks/setup_tasks.js
@@ -32,9 +32,9 @@ define('setup_tasks/setup_tasks',
             var classInfo = scope.classes[id];
             var dayLabels = ['星期日', '星期一', '星期二', '星期三', '星期四',
                                '星期五', '星期六'];
-            
-            return scope.departments[classInfo.department_id].name +
-                (dayLabels[classInfo.weekday] || '') + (classInfo.time || '');
+            var dep = (scope.departments || {})[classInfo.department_id] || {};
+            return (dep.name || '') + (dayLabels[classInfo.weekday] || '') + 
+                (classInfo.time || '');
           };
           
         },
