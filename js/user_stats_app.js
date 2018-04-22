5e053ca1b275b5032749f8dc881ab873b9733712
Xipan Xiao <xxp9@hotmail.com>
2018-04-21 21:13:11 -0700

    can filter all users by departments

--- js/user_stats_app.js
+++ js/user_stats_app.js
@@ -30,64 +30,71 @@ define('user_stats_app', [
               $scope.user = user;
             });
 
-            $scope.classYearChanged = function() {
+            function filterUsers() {
               $scope.users = utils.where($scope.allUsers, function(user) {
-                var classYear = $scope.classes[user.classId].start_year;
-                return $scope.classYears[classYear].checked;
+                var classInfo = classes[user.classId];
+                return $scope.classYears[classInfo.start_year].checked &&
+                    $scope.departments[classInfo.department_id].checked;
               });
+              return utils.truePromise();
+            }
+            $scope.classYearChanged = function() {
+              filterUsers();
+            };
+            
+            $scope.depChanged = function() {
+              filterUsers();
             };
 
+            var classes;
             /// Initializes the [classYears] list by aggregating over all
             /// [users].
-            $scope.aggregateClassYears = function() {
+            function aggregateClassYears() {
               var classIds = {};
-              utils.forEach($scope.users, function(user) {
+              utils.forEach($scope.allUsers, function(user) {
                 classIds[user.classId] = user.classId;
               });
 
-              return rpc.get_classes().then(function(response) {
-                var classYears = {};
-                utils.forEach(classIds, function(classId) {
-                  $scope.classes = response.data;
-                  var start_year = response.data[classId].start_year;
-                  if (classYears[start_year]) return;
-                  classYears[start_year] = {
-                    label: start_year,
-                    checked: true
-                  };
-                });
-                return $scope.classYears = classYears;
+              var classYears = {};
+              utils.forEach(classIds, function(classId) {
+                var start_year = classes[classId].start_year;
+                if (classYears[start_year]) return;
+                classYears[start_year] = {
+                  label: start_year,
+                  checked: true
+                };
               });
+              $scope.classYears = classYears;
+              return utils.truePromise();
             };
+            
+            function getDepartments() {
+              return rpc.get_departments().then(function(response) {
+                $scope.departments = response.data;
+                utils.forEach($scope.departments, function(dep) {
+                  dep.label = dep.name;
+                  dep.checked = dep.id != 9;// dep 9 have all quit people.
+                });
+                return $scope.departments;
+              });
+            }
 
-            $scope.depChanged = function(departmentId) {
-              var classIds;
-              
+            function loadAllUsers() {              
               function getClassIds() {
-                return rpc.get_classes(null, departmentId)
-                    .then(function(response) {
-                  // Exclude '退出' classes from the all departments results.
-                  if (!departmentId) {
-                    function isNotQuit(classInfo) {
-                      return parseInt(classInfo.department_id) != 9; 
-                    }
-                    var classes = utils.where(response.data, isNotQuit);
-                    return classIds = utils.keys(classes);
-                  }
-                  return classIds = utils.keys(response.data);
+                return rpc.get_classes().then(function(response) {
+                  return classes = response.data;
                 });
               }
               
-              $scope.users = [];
               $scope.allUsers = [];
               var requests = [getClassIds];
               function collectClassUsers() {
                 var requests = [];
-                utils.forEach(classIds, function(classId) {
+                utils.forEach(classes, function(classInfo) {
                   requests.push(function() {
-                    return rpc.get_users(null, classId).then(function(response) {
+                    return rpc.get_users(null, classInfo.id).then(function(response) {
                       return $scope.users = $scope.allUsers = 
-                          $scope.users.concat(utils.toList(response.data));
+                          $scope.allUsers.concat(utils.toList(response.data));
                     });
                   });
                 });
@@ -95,10 +102,10 @@ define('user_stats_app', [
               }
               
               utils.requestOneByOne([getClassIds, collectClassUsers, 
-                  $scope.aggregateClassYears]);
+                  getDepartments, aggregateClassYears, filterUsers]);
             };
             
-            $scope.depChanged();
+            loadAllUsers();
           }
         };
       }).config( ['$compileProvider', function( $compileProvider ) {
