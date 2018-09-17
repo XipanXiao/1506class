8525907c64b8838860d34b5acb23d2eda0327675
XipanXiao <xxp9@hotmail.com>
2018-09-16 18:55:01 -0700

    remove the 'shipped' tab

--- order_admin.js
+++ order_admin.js
@@ -30902,14 +30902,14 @@ define('order_details/order_details', [
           order: '='
         },
         link: function(scope) {
-          scope.statusLabels = {0: '待付款', 1: '已发货', 2: '已付款', 3: '钱货两清'};
+          scope.statusLabels = {0: '待付款', 2: '已付款', 3: '钱货两清'};
           scope.hasSelection = function() {
             function itemSelected(item) { return item.selected; }
             var items = scope.order.items;
             return items.length > 1 && items.some(itemSelected);
           };
         },
-        templateUrl : 'js/order_details/order_details.html?tag=201809161630'
+        templateUrl : 'js/order_details/order_details.html?tag=201809161851'
       };
     });
 });
