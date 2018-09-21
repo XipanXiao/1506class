899d4e74ad9d483b7310cfeb7704d9cc25b1c4de
Xipan Xiao <xxp@rodete-desktop-imager.corp.google.com>
2018-09-20 18:32:11 -0700

    show total price on book lists

--- order_admin.js
+++ order_admin.js
@@ -31401,17 +31401,17 @@ define('book_list_details/book_list_details',
             reload(classInfo);
           }
         });
-        
+
         function reload() {
           utils.requestOneByOne([getDepartments, getCategories,
               getDepartmentBooks, getBookList]);
         };
-        
+
         scope.removeItem = function(id) {
           delete scope.classInfo.books[id];
           scope.dirty = true;
         };
-        
+
         scope.clear = function() {
           scope.classInfo.books = {};
           scope.classInfo.adding = true;
@@ -31444,15 +31444,15 @@ define('book_list_details/book_list_details',
 
         /// Adds all selected books to the class's book list.
         scope.addSelectedBooks = function() {
-        	  var select = element[0].querySelector('.book-selection');
-        	  for (let option of select.options) {
-        		if (option.selected) {
-        		  scope.classInfo.books[option.value] = scope.items[option.value];
-              scope.dirty = true;
-        		}
-        	  }
+          var select = element[0].querySelector('.book-selection');
+          for (let option of select.options) {
+            if (option.selected) {
+             scope.classInfo.books[option.value] = scope.items[option.value];
+             scope.dirty = true;
+            }
+          }
         };
-        
+
         /// Updates or creates a book, adds it to [items] if it's new.
         scope.updateBook = function() {
           rpc.update_item(scope.classInfo.editingBook).then(function(response) {
@@ -31483,12 +31483,12 @@ define('book_list_details/book_list_details',
           return category && parseInt(category.parent_id) == 1;
         }
         function convertNumbers() {
-        	  utils.forEach(scope.classInfo.books, function(book) {
-        		book.category = parseInt(book.category);
-        		book.price = parseFloat(book.price);
-        		book.shipping = parseFloat(book.shipping);
-        		book.int_shipping = parseFloat(book.int_shipping);
-        	  });
+          utils.forEach(scope.classInfo.books, function(book) {
+            book.category = parseInt(book.category);
+            book.price = parseFloat(book.price);
+            book.shipping = parseFloat(book.shipping);
+            book.int_shipping = parseFloat(book.int_shipping);
+          });
         }
         function getDepartmentBooks() {
           var dep = scope.departments[scope.classInfo.department_id];
@@ -31515,7 +31515,7 @@ define('book_list_details/book_list_details',
           scope.dirty = false;
         }
       },
-      templateUrl : 'js/book_list_details/book_list_details.html?tag=201809181806'
+      templateUrl : 'js/book_list_details/book_list_details.html?tag=201809201806'
     };
   });
 });
