57704e3c24b658441dfb69c284125fc12ad48327
XipanXiao <xxp9@hotmail.com>
2018-09-22 22:56:00 -0700

    timestamp

--- js/orders/orders.js
+++ js/orders/orders.js
@@ -481,7 +481,7 @@ define('orders/orders', [
           $rootScope.$on('reload-orders', scope.reload);
           scope.$watch('user', scope.reload);
         },
-        templateUrl : 'js/orders/orders.html?tag=201809222314'
+        templateUrl : 'js/orders/orders.html?tag=201809222255'
       };
     });
 });
