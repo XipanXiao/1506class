6be6c05ed15ff0fb1e1255c911ec98c147b82307
xxp9 <xxp9@xxp-macbookpro2.roam.corp.google.com>
2018-12-20 22:25:28 -0800

    add user_input to dependencies

--- local.js
+++ local.js
@@ -31845,6 +31845,103 @@ define('permission_editor/permission_editor',
       };
     });
 });
+window.userInputCache = window.userInputCache || {};
+
+define('user_input/user_input', ['services'], function() {
+  var users = window.userInputCache;
+  
+  return angular.module('UserInputModule', ['ServicesModule'])
+	.directive('userTypeAhead', function(rpc) {
+    return {
+      require: 'ngModel',
+      restrict: 'A',
+      link: function(scope, element, attrs, ngModel) {
+        var searchTimer;
+
+        ngModel.$formatters.push(function(userId) {
+          return users[userId];
+        });
+        ngModel.$parsers.push(function(userLabel) {
+          if (!userLabel) {
+            scope.$parent.update(0);
+            return 0;
+          }
+
+          var userId = users[userLabel];
+          if (userId) {
+            scope.$parent.update(userId);
+            return userId;
+          }
+
+          if (searchTimer) {
+            clearTimeout(searchTimer);
+          }
+
+          searchTimer = setTimeout(function() {
+            searchTimer = null;            
+            rpc.searchByName(userLabel).then(function(response) {
+              if (!response.data) return;
+
+              scope.hints = [];
+              response.data.forEach(function(user) {
+                var label = user.nickname ? 
+                    '{0}({1})'.format(user.name, user.nickname) : user.name;
+                label += '-' + user.email;
+                users[user.id] = label;
+                users[label] = user.id;
+                scope.hints.push(user);
+              });
+            });
+          }, 1000);
+        });
+      }
+    };
+  }).directive('userInput', function(rpc) {
+  	return {
+  	  scope: {
+        editing: '@',
+        onChange: '&',
+        userId: '='
+  	  },
+  	  link: function(scope, elements, attrs) {
+  	    scope.editor = {editing: scope.editing};
+        scope.users = users;
+
+  	    function init(userId) {
+  	      if (!userId) return;
+
+  	      if (users[userId]) return;
+
+  	      rpc.getUserLabel(userId).then(function(response) {
+  	        users[userId] = response.data.label;
+  	      });
+  	    }
+  	    if (scope.userId) {
+  	      init(scope.userId);
+  	    }
+  	    scope.$watch('userId', init);
+  
+  	    scope.keyPressed = function(event) {
+  	      if (event.keyCode == 13 || event.keyCode == 27) {
+  	        scope.editor.editing = false;
+  	        event.preventDefault();
+  	      }
+        };
+        
+        scope.edit = () => {
+          if (attrs.disabled) return;
+          scope.editor.editing = true
+        };
+
+        scope.update = (userId) => {
+          scope.userId = userId;
+          scope.onChange();
+        };
+  	  },
+  		templateUrl : 'js/user_input/user_input.html?tag=20181013'
+  	};
+  });
+});
 define('user_editor/user_editor',
     ['services', 'utils',
      'address_editor/address_editor',
