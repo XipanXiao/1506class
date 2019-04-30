1f55d47661c8a3bd74647ec4e40e86d84a8425fa
XipanXiao <xxp9@hotmail.com>
2019-04-29 19:59:20 -0700

    move staff index.html to root

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
+ln -s staff/web/index.html staff.html
 cd staff
 webdev build
-cp .dart_tool/build/generated/staff/web/main.dart.js ./web
+cp .dart_tool/build/generated/staff/web/main.dart.js ../staff.js
+
