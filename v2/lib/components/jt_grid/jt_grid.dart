9e6dbead0bdae162491e052e408929380fe580fd
XipanXiao <xxp9@hotmail.com>
2019-08-20 07:59:29 -0700

    show limit schedule records for jt grid

--- v2/lib/components/jt_grid/jt_grid.dart
+++ v2/lib/components/jt_grid/jt_grid.dart
@@ -1,13 +1,12 @@
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
 import 'package:angular_components/material_icon/material_icon.dart';
-import 'package:v2/components/abstract_task_report/has_selectable.dart';
+import 'package:v2/components/abstract_task_report/abstract_task_att_limit_grid.dart';
 import 'package:v2/components/abstract_task_report/schedule_records_loader.dart';
 import 'package:v2/components/schedule_grid/schedule_grid.dart';
 import 'package:v2/model/auditable.dart';
-import 'package:v2/model/class_info.dart';
 import 'package:v2/model/jt_report_grid.dart';
-import 'package:v2/model/task_data_pair.dart';
+import 'package:v2/model/report_grid.dart';
 import 'package:v2/model/zb_jt_task_data.dart';
 import 'package:v2/services/task_record_service.dart';
 import 'package:v2/services/zb_service.dart';
@@ -24,75 +23,16 @@ import 'package:v2/services/zb_service.dart';
   templateUrl: 'jt_grid.html',
   exports: [AuditState],
 )
-class JtGridComponent extends HasSelectable<TaskDataPair<JtTaskData>> {
-  final ZBService _zbService;
-  final TaskRecordService _taskService;
-  final ScheduleRecordsLoader _scheduleLoader;
-
-  JtGridComponent(this._zbService, this._taskService, this._scheduleLoader);
+class JtGridComponent extends TaskAttLImitGridComponent<JtTaskData> {
+  JtGridComponent(ZBService zbService, TaskRecordService taskService,
+      ScheduleRecordsLoader loader)
+      : super(zbService, taskService, loader);
 
   @override
-  final users = <TaskDataPair<JtTaskData>>[];
-
-  @Input()
-  set classInfo(ClassInfo classInfo) {
-    if (classInfo == null || classInfo == _classInfo) return;
-    _classInfo = classInfo;
-    _classInfo.taskGrid ??= JtTaskGrid(classInfo.id, classInfo.zb_id);
-
-    if (_halfTerm != null) {
-      reload(_halfTerm);
-    }
-  }
-
-  @Input()
-  set halfTerm(int halfTerm) {
-    if (halfTerm == null || halfTerm == _halfTerm) return;
-    if (_classInfo != null) {
-      reload(halfTerm);
-    }
-  }
-
-  JtTaskGrid grid;
-  ClassInfo _classInfo;
-  int _halfTerm;
-
-  int get halfTerm => _halfTerm;
-
   JtTaskData createTaskData(Map<String, dynamic> map) =>
       JtTaskData.fromJson(map);
 
-  Future<void> reload(int halfTerm) async {
-    var grid = _classInfo.taskGrid;
-
-    if (grid.taskData.isEmpty) {
-      var taskData =
-          await _taskService.getTaskDataStats(_classInfo.id, createTaskData);
-      grid.setTaskData(taskData);
-      grid.computeTotals();
-    }
-
-    if (await _zbService.ensureAuthenticated()) {
-      var data = await _zbService.getJtTaskData(grid.pre_classID, halfTerm);
-      grid.setTaskData({halfTerm: data}, zhibei: true);
-      await _scheduleLoader.load(grid, halfTerm);
-    }
-
-    // Initialize the grid object and _halfTerm after everything is loaded.
-    this.grid = grid;
-    this._halfTerm = halfTerm;
-    _initSelection();
-  }
-
-  void _initSelection() {
-    users.clear();
-    var halfTermData = grid.taskData[_halfTerm];
-    if (halfTermData != null) {
-      users.addAll(halfTermData.values);
-      selection.clear();
-      users.where((user) => user.failed).forEach(selection.select);
-    }
-  }
-
-  void report({user}) {}
+  @override
+  ReportGrid<JtTaskData> createTaskGrid(int classId, int pre_classID) =>
+      JtTaskGrid(classId, pre_classID);
 }
--- v2/lib/model/jt_report_grid.dart
+++ v2/lib/model/jt_report_grid.dart
@@ -4,7 +4,7 @@ import 'package:v2/model/zb_jt_task_data.dart';
 class JtTaskGrid extends ReportGrid<JtTaskData> {
   @override
   final gridTypes = GridTypes(
-      report_main_grid: 'jt_grid', attLimitGrid: 'fohao_att_limit_grid');
+      report_main_grid: 'jt_grid', workGrid: 'fohao_att_limit_grid');
 
   JtTaskGrid(int classId, int pre_classID) : super(3, classId, pre_classID);
 
