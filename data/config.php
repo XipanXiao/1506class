6637c257510af5753847da45c16170dc777ad1e1
XipanXiao <XipanXiao@users.noreply.github.com>
2024-03-23 10:29:37 -0700

    Update config.php

--- data/config.php
+++ data/config.php
@@ -30,5 +30,12 @@
     "dbname": "buddcourses",
     "zbServiceUrl": "http://www.zhibei.info/zb"
   },
+  "bicwclass.in": {
+    "servername": "${MYSQL_SERVER}",
+    "socket": null,
+    "username": "${MYSQL_USERNAME}",
+    "password": "${MYSQL_PASSWORD}",
+    "dbname": "${MYSQL_DATABASE}"
+  },
   "closing": "*/?>"
 }
